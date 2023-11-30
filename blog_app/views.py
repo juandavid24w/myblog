27349557299f75869726bd26ce221f0ed5152407
Juan0402 <davidbare0402@gmail.com>
2023-12-01 00:59:47 +0100

    added forms for creating posts and comments

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,10 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from blog_app.models import Post, Comment
+from blog_app.forms import CommentForm, PostForm
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
+
+    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = Post(
+                title=form.cleaned_data['title'],
+                body=form.cleaned_data['body'],
+            )
+            post.save()
+            return HttpResponseRedirect(request.path_info)
+
     context = {
         'posts': posts,
+        'form': PostForm(),
     }
     return render(request, 'blog/index.html', context)
 
@@ -20,10 +35,23 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data['author'],
+                body=form.cleaned_data['body'],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+
     comments = Comment.objects.filter(post=post)
     context = {
         'post': post,
         'comments': comments,
+        'form': CommentForm(),
     }
 
     return render(request, 'blog/detail.html', context)
