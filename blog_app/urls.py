60be65491b0093cb9b0b5d1704993857b2b7063d
Juan0402 <davidbare0402@gmail.com>
2023-12-01 12:19:19 +0100

    added update and delete views and corresponding templates

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_index, name='blog_index'),
-    path('post/<int:pk>/', views.blog_detail, name='blog_detail'),
+    path('blogs/create', views.create_blog, name='create_blog'),
+    path('blogs/<id>/', views.blog_detail, name='blog_detail'),
+    path('blogs/<id>/update', views.update_blog, name='update_blog'),
+    path('blogs/<id>/delete', views.delete_blog, name='delete_blog'),
     path('category/<category>/', views.blog_category, name='blog_category'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from blog_app.models import Post, Comment
 from blog_app.forms import CommentForm, PostForm
 
@@ -19,9 +20,55 @@ def blog_index(request):
 
     context = {
         'posts': posts,
-        'form': PostForm(),
     }
     return render(request, 'blog/index.html', context)
+
+def create_blog(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # Create a new post with the form data
+            post = Post(
+                title=form.cleaned_data['title'],
+                body=form.cleaned_data['body'],
+            )
+            post.save()
+
+            return HttpResponseRedirect(reverse('blog_detail', args=[post.pk]))
+    else:
+        # Render an empty form for creating a new post
+        form = PostForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'blog/create.html', context)
+
+def update_blog(request, id):
+    post = Post.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # Update the existing post with the form data
+            post.title = form.cleaned_data['title']
+            post.body = form.cleaned_data['body']
+            post.save()
+
+            return HttpResponseRedirect(reverse('blog_detail', args=[post.pk]))
+    else:
+        # Populate the form with the existing post data
+        form = PostForm(initial={'title': post.title, 'body': post.body})
+
+    context = {
+        'post': post,
+        'form': form,
+    }
+    return render(request, 'blog/update.html', context)
+
+def delete_blog(request, id):
+    Post.objects.get(id=id).delete()
+    return HttpResponseRedirect(reverse('blog_index'))
 
 def blog_category(request, category):
     posts = Post.objects.filter(
@@ -33,8 +80,8 @@ def blog_category(request, category):
     }
     return render(request, 'blog/category.html', context)
 
-def blog_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+def blog_detail(request, id):
+    post = Post.objects.get(id=id)
     form = CommentForm()
     if request.method == 'POST':
         form = CommentForm(request.POST)
